24a04355204b1b7c88efb527576995f69f66ce7e
hetia <danielh212sc1@gmail.com>
2024-07-10 05:10:13 -0400

    Removed redundant stuff.

--- src/main/java/daniel/mlm/Main.java
+++ src/main/java/daniel/mlm/Main.java
@@ -1,31 +1,5 @@
 package daniel.mlm;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.jsoup.HttpStatusException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.math.RoundingMode;
-import java.net.URL;
-import java.net.URLConnection;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.UUID;
-
 public class Main {
 
     public static void main(String[] args) {
